f2bdef4e0e9c81d5cf9d2b076485221790d81c4a
ICheered <tjeerd992@gmail.com>
2021-09-03 01:22:21 +0200

    Mobile design main page

--- frontend/app/styles/tailwind.config.js
+++ frontend/app/styles/tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: (theme) => ({
+        c: `0 -24px 0 0 ${theme('colors.red.400')}`,
+      }),
+    },
   },
   variants: {
     extend: {},
